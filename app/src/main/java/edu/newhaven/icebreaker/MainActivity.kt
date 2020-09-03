754e3799ed166b80085383066ab0270b4046d10b
chefeleUNH <chefele@newhaven.edu>
2020-09-02 22:51:50 -0400

    added question class and wired up random button

--- app/src/main/java/edu/newhaven/icebreaker/MainActivity.kt
+++ app/src/main/java/edu/newhaven/icebreaker/MainActivity.kt
@@ -1,11 +1,32 @@
 package edu.newhaven.icebreaker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val questionBank = listOf(
+        Question(R.string.question_dream_job),
+        Question(R.string.question_favorite_book),
+        Question(R.string.question_favorite_food),
+        Question(R.string.question_favorite_movie),
+        Question(R.string.question_favorite_book),
+        Question(R.string.question_thrones)
+    )
+
+    private var currentIndex = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // set the initial question
+        txtQuestion.setText(questionBank.random().textResId)
+
+        // setup event to generate a new question on demand
+        btnGetNewQuestion.setOnClickListener {
+            txtQuestion.setText(questionBank.random().textResId)
+        }
     }
 }
---
+++ app/src/main/java/edu/newhaven/icebreaker/Question.kt
@@ -0,0 +1,5 @@
+package edu.newhaven.icebreaker
+
+import androidx.annotation.StringRes
+
+data class Question(@StringRes val textResId: Int)
